e13553516c3e4b0775cd76f32b77d8c4781320df
Rafael Angelo <rcl.angelo@gmail.com>
2018-09-27 22:09:23 -0400

    initial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public bool Move(Vector2 direction)
+    {
+        return true;
+    }
+
+    bool Blocked(Vector3 position, Vector2 direction)
+    {
+        return false;
+    }
+}
