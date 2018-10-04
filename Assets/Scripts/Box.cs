19baf542b9ee0dac968f53455f329b584e34a9ca
Rafael Angelo <rcl.angelo@gmail.com>
2018-10-03 20:42:13 -0400

    Added level building, all player, box and goal functionality. added UI buttons

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class Box : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool m_OnCross; //true if box has been pushed on to a cross
+
+	public bool Move(Vector2 direction)//Avoid ability to move diagonally
+    {
+        if (BoxBlocked(transform.position, direction))
+        {
+            return false;
+        }
+        else
+        {
+            transform.Translate(direction);//Box not blocked so move it
+            TestForOnCross();
+            return true;
+        }
+    }
+
+
+    public bool BoxBlocked(Vector3 position, Vector2 direction) //Boxes blocked by other boxes and walls
+    {
+        Vector2 newPos = new Vector2(position.x, position.y) + direction;
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+        foreach (var wall in walls)
+        {
+            if (wall.transform.position.x == newPos.x && wall.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        foreach (var box in boxes)
+        {
+            if (box.transform.position.x == newPos.x && box.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void TestForOnCross()
+    {
+        GameObject[] crosses = GameObject.FindGameObjectsWithTag("Cross");
+        foreach (var cross in crosses)
+        {
+            if (transform.position.x == cross.transform.position.x && transform.position.y == cross.transform.position.y)
+            {
+                //On a cross
+                GetComponent<SpriteRenderer>().color = Color.red;
+                m_OnCross = true;
+                return;
+            }
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+        m_OnCross = false;
+    }
+
+
 }
--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class LevelElement //defines each item in a level by mapping a single character (ie #) to a prefab
+{
+    public string m_Character;
+    public GameObject m_Prefab;
+}
+
+
+
 public class LevelBuilder : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int m_CurrentLevel;
+    public List<LevelElement> m_LevelElements;
+    private Level m_Level;
+
+    GameObject GetPrefab(char c)
+    {
+        LevelElement levelElement = m_LevelElements.Find(le => le.m_Character == c.ToString());
+        if (levelElement != null)
+        {
+            return levelElement.m_Prefab;
+        } else
+        {
+            return null;
+        }
+    }
+
+    public void NextLevel()
+    {
+        m_CurrentLevel++;
+        if (m_CurrentLevel >= GetComponent<Levels>().m_Levels.Count)
+        {
+            m_CurrentLevel = 0; //Wrap back to first level
+        }
+    }
+
+    public void Build()
+    {
+        m_Level = GetComponent<Levels>().m_Levels[m_CurrentLevel];
+        //Offset coordinates so that center of level is roughly at 0,0
+        int startx = -m_Level.Width / 2; //Save start x since needs to be reset in loop
+        int x = startx;
+        int y = -m_Level.Height / 2;
+        foreach (var row in m_Level.m_Rows)
+        {
+            foreach (var ch in row)
+            {
+                Debug.Log(ch);
+                GameObject prefab = GetPrefab(ch);
+                if (prefab)
+                {
+                    Debug.Log(prefab.name);
+                    Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+                }
+                x++;
+            }
+            y++;
+            x = startx;
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
